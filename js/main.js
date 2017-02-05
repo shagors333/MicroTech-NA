f725046c4562f7bb2a76deac702acb250d4d426b
ab-rasel <abrasel600@gmail.com>
2017-02-05 16:27:27 +0600

    ok

--- js/main.js
+++ js/main.js
@@ -1,27 +1,5 @@
-$(document).ready(function () {
- $('.Sslider').owlCarousel({
-        items: 3,
-        loop: true,
-        nav: false,
-        dots: false,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            640: {
-                items: 3
-            },
-            1000: {
-                items: 6
-            }
-        }
-    });
+$(document).ready(function() {
+
     
-    /*back to top by shagor*/
-    $("body, html").on("click", "#backToTop", function (e) {
-        e.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 800);
-    });
 });
 
