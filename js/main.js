584d9a2a907fa1d9eab7d7d7da7735f4ef427179
shagors333 <shagors333@gmail.com>
2017-02-05 16:32:04 +0600

    add sub menu success and slider complete

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
- $('.sSlider').owlCarousel({
-        items: 3,
+    $('.sSlider').owlCarousel({
+        items: 1,
         loop: true,
         nav: false,
         dots: false,
+        pagination: false,
+        touchDrag: false,
         responsiveClass: true,
+        navigationText: false,
+        navigation: false,
+        autoPlay: true,
+        mouseDrag: false,
         responsive: {
             0: {
                 items: 1
@@ -17,9 +23,32 @@ $(document).ready(function () {
             }
         }
     });
+    /*FixHeader Start By Shagor */
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 74)
+        {
+            $(".Mfix").addClass('fixedHeader');
+        } else
+        {
+            $(".Mfix").removeClass('fixedHeader');
+        }
+    });
+
+    /*FixHeader End By Shagor */
     
+    /*MobileMenu Start By Shagor */
+    
+    $(".MobileMenu").on('click', function() {
+        $(".Menu > ul").slideToggle('slow');
+    });
+    $(".SubSer").on('click', function() {
+        $(".Menu ul li .Submenu").slideToggle('slow');
+    });
+    
+    /*MobileMenu End By Shagor */
+
     /*back to top by shagor*/
-    $("body, html").on("click", "#backToTop", function (e) {
+    $("body, html").on("click", ".BackTo", function (e) {
         e.preventDefault();
         $('html, body').animate({scrollTop: 0}, 800);
     });
