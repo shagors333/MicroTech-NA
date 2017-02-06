c5b2a53bbabd38ae4133bfbfa8d99033d5cf5389
shagors333 <shagors333@gmail.com>
2017-02-06 15:24:13 +0600

    Complete Footer Common Section
    Completed by shagor

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     });
     /*FixHeader Start By Shagor */
     $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 74)
+        if ($(window).scrollTop() > 0)
         {
             $(".Mfix").addClass('fixedHeader');
         } else
@@ -36,22 +36,67 @@ $(document).ready(function () {
     });
 
     /*FixHeader End By Shagor */
-    
+
     /*MobileMenu Start By Shagor */
-    
-    $(".MobileMenu").on('click', function() {
+    $('.scrolls a').on('click', function () {
+        $('html, body').animate({scrollTop: $(this.hash).offset().top - (100)}, 1000);
+        return false;
+    });
+
+    function Scroll() {
+
+        var contentTop = [];
+        var contentBottom = [];
+        var winTop = $(window).scrollTop();
+        var rangeTop = 200;
+        var rangeBottom = 500;
+
+        $('.Menu').find('.scrolls > a').each(function () {
+            var atr = $(this).attr('href');
+            if ($(atr).length > 0)
+            {
+                contentTop.push($($(this).attr('href')).offset().top(200));
+                contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
+            }
+
+        });
+
+        $.each(contentTop, function (i) {
+
+            if (winTop > contentTop[i] - rangeTop) {
+
+                $('.MainMenu li.scrolls')
+                        .removeClass('active')
+                        .eq(i).addClass('active');
+            }
+        });
+
+    }
+
+    $(".MobileMenu").on('click', function () {
         $(".Menu > ul").slideToggle('slow');
     });
-    $(".SubSer").on('click', function() {
+    $(".SubSer").on('click', function () {
         $(".Menu ul li .Submenu").slideToggle('slow');
     });
-    
+
     /*MobileMenu End By Shagor */
 
     /*back to top by shagor*/
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 100)
+        {
+            $(".BackTo").fadeIn('slow');
+        } else
+        {
+            $(".BackTo").fadeOut('slow');
+        }
+
+    });
     $("body, html").on("click", ".BackTo", function (e) {
         e.preventDefault();
         $('html, body').animate({scrollTop: 0}, 800);
     });
+});
 
 
